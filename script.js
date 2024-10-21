09b2997cd90ed5950cbfa7bed8fcf7fe9d1f0c8b
Hassan-khudhair <hassankhudhairabdali@gmail.com>
2024-10-21 14:35:17 +0300

    update slider size

--- script.js
+++ script.js
@@ -2,10 +2,19 @@ const cardWrapper = document.querySelector('.card-wrapper');
 const cards = document.querySelectorAll('.card');
 const leftBtn = document.getElementById('left-btn');
 const rightBtn = document.getElementById('right-btn');
+
 let currentIndex = 0;
 
 function updateSlider() {
-  cardWrapper.style.transform = `translateX(-${currentIndex * 10}%)`;
+  var width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
+  
+  if(width >= 1000){
+    cardWrapper.style.transform = `translateX(-${currentIndex * 5.5}%)`;
+  }else if(width <= 992 && width >= 767){
+    cardWrapper.style.transform = `translateX(-${currentIndex * 6}%)`;
+  }else if(width <= 767){
+    cardWrapper.style.transform = `translateX(-${currentIndex * 7}%)`;
+  }
 }
 
 rightBtn.addEventListener('click', () => {
